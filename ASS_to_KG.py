b8606580c933032e3ab89b5c25d0c1c0e16fa8c8
NerdTsai <nerdtsai@gmail.com>
2018-11-12 15:27:56 +0800

    Update ASS_to_KG.py
    修复了输出乱码问题

--- ASS_to_KG.py
+++ ASS_to_KG.py
@@ -1,7 +1,9 @@
 import os
+import io
+import sys
 import numpy as np
 import pandas as pd
-
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8') #解决中文输出乱码问题
 
 def find_csv(find_path):
     filename_path_list = []
@@ -29,8 +31,8 @@ def init_matrix(path):
 
 if __name__ == '__main__':
     #--------------------Configuration--Start--------------------------
-    path = 'C:\\Users\\cc_lo\\Desktop\\result\\DedeCms_attack'
-    file_name = 'DedeCms_attack.csv' #Resin_viewfile_attack/DedeCms_attack
+    path = 'C:\\Users\\cc_lo\\Desktop\\result\\Resin_viewfile_attack'
+    file_name = 'Resin_viewfile_attack.csv' #Resin_viewfile_attack/DedeCms_attack
 
     pd.set_option('display.max_columns',None) #df输出显示所有列
     #--------------------Configuration---End---------------------------
@@ -41,18 +43,29 @@ if __name__ == '__main__':
         data = get_ass(file_path)
         for indexs in data.index[0:-1]:
             rows_f = data.loc[indexs].values[0:-1]
-            fT = rows_f[30] #subject,30,告警类型
+            fT = rows_f[30] #subject,30,告警类型,fT为当前处理告警
             rows_n = data.loc[indexs+1].values[0:-1]
             nT = rows_n[30]
+            if fT == nT :
+                continue #不计自身转移概率
             Matrix_tmp[nT][fT] = Matrix_tmp[nT][fT] + 1
     
-    Matrix_P = Matrix_tmp.div((Matrix_tmp.apply(sum,axis=1)),axis=0).fillna(0)
+    Matrix_P = Matrix_tmp.div((Matrix_tmp.apply(sum,axis=1)),axis=0).fillna(0) #得到了转移概率矩阵
 
-    #Matrix_P.to_csv(file_name,index=True,header=True) #csv输出有误
-    #np.savetxt(file_name,Matrix_P,fmt="%.2f",delimiter=",")
-    print (Matrix_tmp)
-    
-
-    
-    
+    #----------------
+    for lines in Matrix_P.index:#lines是行名
+        Max_P_Array = Matrix_P.loc[lines].values[0:-1]
+        col_list = Matrix_P.columns.values.tolist()
+        max_tmp = max(Max_P_Array)
         
+        if max_tmp <= 0.5 :
+            continue
+        columns_num = np.argwhere(Max_P_Array == max_tmp)
+        columns = col_list[columns_num[0][0]]
+        print (lines + '->' + columns + '---------' + str(max_tmp))
+    #----------------输出概率大于0.5的因果关系
+    
+
+    #Matrix_P.to_csv(file_name,index=True,header=True,encoding='utf_8_sig',float_format='%.3f') #csv输出有误
+
+    #print (Matrix_P)
